91b7232a79138c9b9870f4274c88afdc4633edf9
Espi <smol@espi.xyz>
2020-04-08 00:47:38 -0400

    Roleplay cmds, WIP avatar cmd
    TODO - fix aliases, clientperms, and find a way to return dynamicUserthingy on msg.author - it works when done msg.author.thingy but not in pargs

--- cmds/Core/avatar.js
+++ cmds/Core/avatar.js
@@ -1,17 +1,47 @@
 const Command = require("../../lib/structures/Command");
+const format = require("../../lib/scripts/Format");
 
 class avatarCommand extends Command {
   constructor(...args) {
     super(...args, {
+      args: "[user:user&fallback]",
       aliases: ["pfp", "profilepic", "profilepicture", "uicon", "usericon"],
-      args: "<:user:>",
       description: "Displays a user's avatar.",
       cooldown: 3,
     });
   }
 
-  async run(msg, args) {
-    msg.channel.createMessage(this.bot.embed("lol retard", "idiot", "error"));
+  // todo - remove if else, find a way to make it one (pargs[0].value.user doesnt work on msg.author)
+  async run(msg, args, pargs) {
+    // Sends the author's avatar
+    if (!pargs[0].value.user) {
+      msg.channel.createMessage({
+        embed: {
+          color: this.bot.embed.colour("general"),
+          author: {
+            icon_url: pargs[0].value.dynamicAvatarURL(null, 1024),
+            name: format.tag(pargs[0].value, false),
+          },
+          image: {
+            url: pargs[0].value.dynamicAvatarURL(null, 1024),
+          },
+        },
+      });
+    } else {
+      // Sends the user's avatar
+      msg.channel.createMessage({
+        embed: {
+          color: this.bot.embed.colour("general"),
+          author: {
+            icon_url: pargs[0].value.user.dynamicAvatarURL(null, 1024),
+            name: format.tag(pargs[0].value.user, false),
+          },
+          image: {
+            url: pargs[0].value.user.dynamicAvatarURL(null, 1024),
+          },
+        },
+      });
+    }
   }
 }
 
