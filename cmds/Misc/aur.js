7fb41cc42e55bbbf728e1a1b6866f98daeb1cbcc
Espi <espi@lesbian.codes>
2020-04-15 16:55:51 -0400

    Command logging, dm handling, cleanup

--- cmds/Misc/aur.js
+++ cmds/Misc/aur.js
@@ -1,39 +1,36 @@
+// todo: limit aur results to top 15... or pagify it?
 const Command = require("../../lib/structures/Command");
+const Wait = require("../../lib/utils/Wait");
 const fetch = require("node-fetch");
-const waitFor = require("../../lib/utils/waitFor");
 const format = require("../../lib/scripts/Format");
 
 class aurCommand extends Command {
   constructor(...args) {
     super(...args, {
       description: "Looks up packages on the AUR.",
-      args: "<name:string>",
+      args: "<package:string>",
       cooldown: 2,
     });
   }
 
   async run(msg, args) {
+    // Fetches the API
     let res = await fetch(`https://aur.archlinux.org/rpc/?v=5&type=search&arg=${args.join(" ")}`);
     res = await res.json();
-    console.log(res.results);
     let pkg;
-    if (res.resultcount == 1) {
+    if (res.resultcount === 1) {
       pkg = res.results[0];
     } else if (res.resultcount > 1) {
-      let emsg = await msg.channel.createMessage({
-        embed: {
-          title: "Multiple packages match your keyword",
-          description: res.results.map((r, i) => `**${i + 1}**: ${r.Name} (${r.Popularity.toFixed(2)}%)`).join("\n"),
-        },
-      })
-      await waitFor("messageCreate", 60000, async (m) => {
-        if (m.author.id != msg.author.id) return;
-        if (m.channel.id != msg.channel.id) return;
+      let emsg = await msg.channel.createMessage(this.bot.embed("📦 Multiple Results", res.results.map((r, i) => `**${i + 1}**: ${r.Name} (${r.Popularity.toFixed(2)}%)`).join("\n")))
+      // Wait event
+      await Wait("messageCreate", 60000, async (m) => {
+        if (m.author.id !== msg.author.id) return;
+        if (m.channel.id !== msg.channel.id) return;
         if (!m.content) return;
-        let foundpkg = isNaN(m.content) ? res.results.find(r => r.Name.toLowerCase() == m.content.toLowerCase()) : res.results[parseInt(m.content) - 1];
-
+        let foundpkg = isNaN(m.content) ? res.results.find(r => r.Name.toLowerCase() === m.content.toLowerCase()) : res.results[parseInt(m.content) - 1];
+        // Invalid
         if (!foundpkg) {
-          let message = await msg.channel.createMessage(this.bot.embed("❌ Invalid", "invalid thing", "error"));
+          let message = await msg.channel.createMessage(this.bot.embed("❌ Error", "Invalid package", "error"));
           setTimeout(() => {
             message.delete();
           }, 2000);
@@ -41,45 +38,32 @@ class aurCommand extends Command {
         }
         pkg = foundpkg;
         return true;
-      }, this.bot).catch(err => err.message == "timeout" && emsg.edit(this.bot.embed("timeout", "1 min timeout ran out", "error")));
+        // Timeout
+      }, this.bot).catch(err => err.message === "timeout" && emsg.edit(this.bot.embed("❌ Error", "The **1 minute timeout** has been reached.", "error")));
     }
 
-    if (!pkg) return msg.channel.createMessage("no pkg");
-
+    // No package found
+    if (!pkg) return msg.channel.createMessage(this.bot.embed("❌ Error", "No packages found.", "error"));
     let pkginfo = await fetch(`https://aur.archlinux.org/rpc/?v=5&type=info&arg=${pkg.Name}`);
     pkginfo = await pkginfo.json();
-    pkginfo = pkginfo.results.find(p => p.Name == pkg.Name);
+    pkginfo = pkginfo.results.find(p => p.Name === pkg.Name);
 
+    // Sets the fields
+    let fields = [];
+    if (pkg.numvotes) fields.push({ name: "Votes", value: pkg.NumVotes, inline: true });
+    if (pkg.Popularity) fields.push({ name: "Popularity", value: pkg.Popularity.toFixed(2), inline: true });
+    if (pkg.Maintainer) fields.push({ name: "Maintainer", value: pkg.Maintainer, inline: true });
+    if (pkg.FirstSubmitted) fields.push({ name: "Submitted", value: format.date(pkg.FirstSubmitted * 1000), inline: true });
+    if (pkg.LastModified) fields.push({ name: "Updated", value: format.date(pkg.LastModified * 1000), inline: true });
+    fields.push({ name: "Dependencies", value: [...pkginfo.Depends, ...pkginfo.MakeDepends].join(", ") || "None" })
+    // Sends the embed
     msg.channel.createMessage({
       embed: {
-        title: `${pkg.Name} ${pkg.Version}`,
+        title: `📦 ${pkg.Name} ${pkg.Version}`,
         description: pkg.Description,
-        fields: [{
-          name: "Votes",
-          value: pkg.NumVotes,
-          inline: true
-        }, {
-          name: "Popularity",
-          value: pkg.Popularity.toFixed(2),
-          inline: true
-        }, {
-          name: "Maintainer",
-          value: pkg.Maintainer || "None",
-          inline: true
-        }, {
-          name: "First Submitted",
-          value: format.date(pkg.FirstSubmitted * 1000),
-          inline: true
-        }, {
-          name: "Last Updated",
-          value: format.date(pkg.LastModified * 1000),
-          inline: true
-        }, {
-          name: "Dependencies",
-          value: [...pkginfo.Depends, ...pkginfo.MakeDepends].join(", ") || "None",
-          inline: true
-        }]
-      }
+        color: this.bot.embed.colour("general"),
+        fields: fields,
+      },
     })
   }
 }
