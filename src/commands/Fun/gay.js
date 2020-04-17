e9ad78376548d5696c8fc5121fc8def698035d0d
cth103 <cthdev@email.com>
2020-04-17 22:39:43 +0100

    Add an Easter Egg

--- src/commands/Fun/gay.js
+++ src/commands/Fun/gay.js
@@ -14,6 +14,7 @@ class gayCommand extends Command {
     // Random 1 - 100%
     let random = Math.floor(Math.random() * 99) + 1
     if (user.id === "647269760782041133") return msg.channel.createMessage(this.bot.embed("🏳️‍🌈 Lesbiab", `**${user.username}** is the gayest girl, ever. 💜💙`));
+    if (user.id === "284432595905675264") return msg.channel.createMessage(this.bot.embed("CEO", `**${user.username}** is CEO`));
     msg.channel.createMessage(this.bot.embed("🏳️‍🌈 Gay", `**${user.username}** is **${random}%** gay!`));
   }
 }
