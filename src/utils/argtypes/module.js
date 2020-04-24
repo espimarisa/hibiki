c703b7d84cebececadb0581aacd00f1504987bec
Espi <espi@lesbian.codes>
2020-04-24 15:45:05 -0400

    Arg parsing tweaks
    Co-Authored-By: resolved <resolvedxd@gmail.com>

---
+++ src/utils/argtypes/module.js
@@ -0,0 +1,4 @@
+module.exports = [function command(a, msg, flag) { this.bot.commands.find(c => flag == "strict" ? c.id == a : c.id.startsWith(a) || (c.aliases && c.aliases.includes(a))) },
+  function event(a, msg, flag) { this.bot.events.find(e => flag == "strict" ? c.id == a : e.id.toLowerCase().startsWith(a)) },
+  function module(a) { this.bot.commands.find(c => c.id.toLowerCase().startsWith(a) || (c.aliases && c.aliases.includes(a))) },
+];
