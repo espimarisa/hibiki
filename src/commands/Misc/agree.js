a1f7aede366b4eea6c30abf6e785c04d93bf1b52
Espi Marisa <hi@espi.me>
2020-04-28 23:32:27 -0400

    Agree, clean, and softban commands

---
+++ src/commands/Misc/agree.js
@@ -0,0 +1,29 @@
+const Command = require("../../lib/structures/Command");
+
+class agreeCommand extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Gives you the set agree role.",
+    });
+  }
+
+  async run(msg) {
+    // Gets guildcfg
+    let guildcfg = await this.bot.db.table("guildcfg").get(msg.channel.guild.id);
+    if (!guildcfg) guildcfg = { id: msg.channel.guild.id };
+    if (!guildcfg.agreeChannel) return;
+    // Sets channel; looks for role
+    const agreeChannel = await msg.channel.guild.channels.find(c => c.id === guildcfg.agreeChannel);
+    if (!agreeChannel) return;
+    if (msg.channel.id !== agreeChannel.id) return;
+    const agreeRole = await msg.channel.guild.roles.find(r => r.id === guildcfg.agreeRole);
+    if (!agreeRole) return;
+    const memberRole = await msg.member.roles.includes(agreeRole.id);
+    if (memberRole === true) return;
+    // Adds the role
+    await msg.member.addRole(agreeRole.id, "Ran the agree command").catch(() => {});
+    await msg.delete().catch(() => {});
+  }
+}
+
+module.exports = agreeCommand;
---
+++ src/commands/Moderation/clean.js
@@ -0,0 +1,26 @@
+const Command = require("../../lib/structures/Command");
+
+class agreeCommand extends Command {
+  constructor(...args) {
+    super(...args, {
+      description: "Deletes the last 10 messages from the bot.",
+      requiredperms: "manageMessages",
+      staff: true,
+    });
+  }
+
+  async run(msg) {
+    // Gets the messages
+    let msgs = await msg.channel.getMessages(100);
+    msgs = msgs.filter(m => m.author.id === this.bot.user.id);
+    msgs = msgs.map(m => m.id);
+    msgs.splice(msgs.length - 10, msgs.length);
+    // Deletes the messages
+    msg.channel.deleteMessages(msgs).catch(() => {});
+    const cleanmsg = await msg.channel.createMessage(this.bot.embed("💣 Clean", "Deleted the **last 10** messages from me."));
+    // Deletes success message
+    setTimeout(() => { cleanmsg.delete().catch(() => {}); }, 2000);
+  }
+}
+
+module.exports = agreeCommand;
