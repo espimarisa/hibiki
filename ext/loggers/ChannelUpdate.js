44b1a4e948751a77db56a68d32bb6cead2cb6814
Espi <espi@lesbian.codes>
2020-05-03 16:41:12 -0400

    Channel creation/deletion/update logging

--- ext/loggers/ChannelUpdate.js
+++ ext/loggers/ChannelUpdate.js
@@ -1,3 +1,144 @@
 /*
   This logs when a channel is updated.
 */
+
+const Logging = require("../../lib/structures/Logging");
+const format = require("../../lib/scripts/Format");
+
+module.exports = (bot) => {
+  // Logging database
+  const loggingdb = new Logging(bot.db);
+  const cansend = async (guild) => {
+    if (!guild || !guild.channels) return;
+    const canlog = await loggingdb.canLog(guild);
+    if (!canlog) return;
+    // Sets type
+    const channel = await loggingdb.guildlogging(guild, "eventLogging");
+    if (guild.channels.has(channel)) return channel;
+  };
+
+  // Logs when a channel is created
+  bot.on("channelCreate", async channel => {
+    // Finds channel; returns if it shouldn't log
+    if (channel.type === 1 || channel.type === 3) return;
+    const logchannel = await cansend(channel.guild, "channelCreate");
+    if (!logchannel) return;
+    const embed = {
+      color: bot.embed.colour("general"),
+      description: `<#${channel.id}> (${channel.id})`,
+      author: {
+        name: `Channel Created`,
+      },
+    };
+    // Reads the audit logs
+    const logs = await channel.guild.getAuditLogs(1, null, 10).catch(() => {});
+    if (logs) {
+      const log = logs.entries[0];
+      const user = logs.users[0];
+      // Adds to the embed
+      if (log && new Date().getTime() - new Date(log.id / 4194304 + 1420070400000).getTime() < 3000) {
+        embed.author.name = `${format.tag(user)} created a channel.`;
+        embed.author.icon_url = user.avatarURL;
+      }
+    }
+    bot.createMessage(logchannel, { embed: embed }).catch(() => {});
+  });
+
+  // Logs when a channel is deleted
+  bot.on("channelDelete", async channel => {
+    // Finds channel; returns if it shouldn't log
+    if (channel.type === 1 || channel.type === 3) return;
+    const logchannel = await cansend(channel.guild, "channelCreate");
+    if (!logchannel) return;
+    const embed = {
+      color: bot.embed.colour("error"),
+      description: `**ID:** ${channel.id}`,
+      author: {
+        name: `#${channel.name} deleted`,
+      },
+    };
+    // Reads the audit logs
+    const logs = await channel.guild.getAuditLogs(1, null, 12).catch(() => {});
+    if (logs) {
+      const log = logs.entries[0];
+      const user = logs.users[0];
+      // Adds to the embed
+      if (log && new Date().getTime() - new Date(log.id / 4194304 + 1420070400000).getTime() < 3000) {
+        embed.author.name = `${format.tag(user)} deleted #${channel.name}.`;
+        embed.author.icon_url = user.avatarURL;
+      }
+    }
+    bot.createMessage(logchannel, { embed: embed }).catch(() => {});
+  });
+
+  // Logs when a channel is updated
+  bot.on("channelUpdate", async (channel, oldchannel) => {
+    // Finds channel; returns if it shouldn't log
+    if (channel.type === 1 || channel.type === 3) return;
+    const logchannel = await cansend(channel.guild, "channelUpdate");
+    if (!logchannel) return;
+    const embed = {
+      color: bot.embed.colour("general"),
+      author: {
+        name: `#${oldchannel.name} edited`,
+      },
+      fields: [],
+    };
+
+    // Channel name differences
+    if (channel.name !== oldchannel.name) {
+      embed.fields.push({
+        name: "Name",
+        value: `${oldchannel.name} ➜ ${channel.name}`,
+      });
+    }
+
+    // Topic differences
+    if (channel.topic !== oldchannel.topic) {
+      embed.fields.push({
+        name: "Topic",
+        value: `${oldchannel.topic} ➜ ${channel.topic}`,
+      });
+    }
+
+    // NSFW differences
+    if (channel.nsfw !== oldchannel.nsfw) {
+      embed.fields.push({
+        name: "NSFW",
+        value: `${channel.nsfw ? "Enabled" : "Disabled"} ➜ ${oldchannel.nsfw ? "Enabled" : "Disabled"}`,
+      });
+    }
+
+    // Bitrate differences
+    if (channel.bitrate && channel.bitrate !== oldchannel.bitrate) {
+      embed.fields.push({
+        name: "Bitrate",
+        value: `${oldchannel.bitrate} ➜ ${channel.bitrate}`,
+      });
+    }
+
+    // Slowmode differences
+    if (channel.rateLimitPerUser !== oldchannel.rateLimitPerUser) {
+      embed.fields.push({
+        name: "Slowmode",
+        value: `${oldchannel.rateLimitPerUser === 0 ? "No cooldown" : `${oldchannel.rateLimitPerUser} seconds`} ➜ ${channel.rateLimitPerUser === 0 ?
+          "No cooldown" : `${channel.rateLimitPerUser} seconds`}`,
+      });
+    }
+
+    if (!embed.fields.length) return;
+    // Reads the audit logs
+    const logs = await channel.guild.getAuditLogs(1, null, 11).catch(() => {});
+    if (logs) {
+      // Updates embed if needed
+      const log = logs.entries[0];
+      const user = logs.users[0];
+      if (log && new Date().getTime() - new Date(log.id / 4194304 + 1420070400000).getTime() < 3000) {
+        embed.author.name = `${format.tag(user)} edited #${oldchannel.name}.`;
+        embed.author.icon_url = user.avatarURL;
+      }
+    }
+    // Sends the embed
+    bot.createMessage(logchannel, { embed: embed }).catch(() => {});
+  });
+};
