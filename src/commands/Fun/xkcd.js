5ff624058f96dac9f9c360e3ead0b69b25bcbfd7
Espi Marisa <hi@espi.me>
2020-06-18 14:29:43 -0400

    feat: fetch any xkcd comic now

--- src/commands/Fun/xkcd.js
+++ src/commands/Fun/xkcd.js
@@ -4,27 +4,66 @@ const fetch = require("node-fetch");
 class xkcdCommand extends Command {
   constructor(...args) {
     super(...args, {
-      description: "Sends a random comic from XKCD.",
+      args: "[id:string]",
+      description: "Sends a comic from XKCD.",
       allowdms: true,
       cooldown: 3,
     });
   }
 
-  async run(msg) {
-    // Fetches the info file
-    let res = await fetch("http://xkcd.com/info.0.json");
-    const newest = await res.json();
-    // Sends if no comic
-    if (!newest) return msg.channel.createMessage(this.bot.embed("❌ Error", "Couldn't send the comic. Try again later.", "error"));
-    // Gets a random comic
-    const random = Math.floor(Math.random() * newest.num) + 1;
-    res = await fetch(`https://xkcd.com/${random}/info.0.json`);
-    const body = await res.json();
+  async run(msg, args) {
+    // Gets the day's comic
+    const comic = args.join("");
+    if (comic === "today") {
+      const body = await fetch("https://xkcd.com/info.0.json").then(async res => res.json());
+      if (!body || !body.num) return this.bot.embed("❌ Error", "Couldn't send the comic. Try again later.", msg, "error");
 
-    // Sends the embed
+      return msg.channel.createMessage({
+        embed: {
+          title: `💬 ${body.safe_title}`,
+          description: `${body.alt}`,
+          color: this.bot.embed.color("general"),
+          image: {
+            url: body.img,
+          },
+          footer: {
+            icon_url: this.bot.user.dynamicAvatarURL(),
+            text: `Ran by ${this.bot.tag(msg.author)}`,
+          },
+        },
+      });
+    }
+
+    // Gets specified comic
+    if (args.length) {
+      const body = await fetch(`https://xkcd.com/${comic}/info.0.json`).then(async res => await res.json().catch(() => {}));
+      if (!body || !body.num) return this.bot.embed("❌ Error", "Comic not found.", msg, "error");
+
+      return msg.channel.createMessage({
+        embed: {
+          title: `💬 ${body.safe_title}`,
+          description: `${body.alt}`,
+          color: this.bot.embed.color("general"),
+          image: {
+            url: body.img,
+          },
+          footer: {
+            icon_url: this.bot.user.dynamicAvatarURL(),
+            text: `Ran by ${this.bot.tag(msg.author)}`,
+          },
+        },
+      });
+    }
+
+    // Random comic
+    let body = await fetch("https://xkcd.com/info.0.json").then(async res => res.json());
+    if (!body || !body.num) return this.bot.embed("❌ Error", "Couldn't send the comic. Try again later.", msg, "error");
+    const random = Math.floor(Math.random() * body.num) + 1;
+    body = await fetch(`https://xkcd.com/${random}/info.0.json`).then(async res => res.json());
+
     msg.channel.createMessage({
       embed: {
-        title: `💭 ${body.safe_title}`,
+        title: `💬 ${body.safe_title}`,
         description: `${body.alt}`,
         color: this.bot.embed.color("general"),
         image: {
@@ -32,7 +71,7 @@ class xkcdCommand extends Command {
         },
         footer: {
           icon_url: this.bot.user.dynamicAvatarURL(),
-          text: `Published on ${body.day <= 10 ? `0${body.day}` : body.day}/${body.month <= 10 ? `0${body.month}` : body.day}/${body.year}`,
+          text: `Ran by ${this.bot.tag(msg.author)}`,
         },
       },
     });
