2dc5a1986c698967ffb76da8104bbe10941e6e83
Espi <espi@lesbian.codes>
2020-06-18 21:04:11 -0400

    feat: re-add default emoji enlarging (@cthpw103)

--- src/commands/Misc/emoji.js
+++ src/commands/Misc/emoji.js
@@ -10,17 +10,61 @@ class emojiCommand extends Command {
   }
 
   run(msg, args) {
-    const emojiid = args.join("").split(/<:[a-zA-Z]{1,128}:([0-9]{17,18})>/).join("");
-    if (!emojiid || isNaN(emojiid)) {
-      return msg.channel.createMessage(this.bot.embed("❌ Error", "Either that's a default emoji or it wasn't found.", "error"));
+    const emoji = args.join("").match(/(?<=<a?:.*:)\d*(?=>)/);
+    const animated = /<[a]:/g.test(args.join(""));
+
+    // Unicode to emoji
+    function toEmoji(string) {
+      const characters = [];
+      let charCode = 0;
+      let sgFix = 0;
+
+      for (let i = 0; i < string.length; i++) {
+        charCode = string.charCodeAt(i);
+        if (sgFix) {
+          characters.push((0x10000 + (sgFix - 0xD800 << 10) + (charCode - 0xDC00)).toString(16));
+          sgFix = 0;
+        } else if (charCode >= 0xD800 && charCode <= 0xDBFF) {
+          sgFix = charCode;
+        } else {
+          characters.push(charCode.toString(16));
+        }
+      }
+      return characters.join("-");
     }
 
+    // Default Discord emojis
+    if (!emoji || isNaN(emoji)) {
+      if (/\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff]/.test(args[0])) {
+        return msg.channel.createMessage({
+          embed: {
+            title: "😄 Emoji",
+            color: this.bot.embed.color("general"),
+            image: {
+              url: `https://twemoji.maxcdn.com/v/13.0.0/72x72/${toEmoji(args[0])}.png`,
+            },
+            footer: {
+              text: `Ran by ${this.bot.tag(msg.author)}`,
+              icon_url: msg.author.dynamicAvatarURL(),
+            },
+          },
+        });
+      }
+
+      return this.bot.embed("❌ Error", "No **emoji** was provided.", msg, "error");
+    }
+
+    // Custom emoji
     msg.channel.createMessage({
       embed: {
         title: "😄 Emoji",
         color: this.bot.embed.color("general"),
         image: {
-          url: `https://cdn.discordapp.com/emojis/${emojiid}.png`,
+          url: `https://cdn.discordapp.com/emojis/${emoji}.${animated ? "gif" : "png"}`,
+        },
+        footer: {
+          text: `Ran by ${this.bot.tag(msg.author)}`,
+          icon_url: msg.author.dynamicAvatarURL(),
         },
       },
     });
