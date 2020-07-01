788913bf2a8b2dd3a5f320bd57bd90d7cf48b3de
Espi <espi@lesbian.codes>
2020-07-01 18:16:27 -0400

    refactor: use default args for currency command

--- src/commands/Misc/currency.js
+++ src/commands/Misc/currency.js
@@ -5,40 +5,33 @@ class currencyCommand extends Command {
   constructor(...args) {
     super(...args, {
       aliases: ["convert", "convertmoney"],
-      args: "<amount:string> [from:string] [to:string]",
+      args: "<amount:string> <from:string> <to:string>",
       description: "Converts money from one currency to another",
       cooldown: 3,
     });
   }
 
-  async run(msg, [amount, from, to]) {
-    if (isNaN(amount)) {
-      to = from;
-      from = amount;
-    }
+  async run(msg, args) {
+    const from = args[0];
+    const base = args[1];
+    const to = args[2];
 
     if (!from) return this.bot.embed("❌ Error", "No amount to **convert from** was provided.", msg, "error");
+    if (!base || !isNaN(base)) return this.bot.embed("❌ Error", "No **base currency** was provided.", msg, "error");
     if (!to) return this.bot.embed("❌ Error", "No amount to **convert to** was provided.", msg, "error");
 
     const body = await fetch(
-      `https://api.exchangeratesapi.io/latest?base=${encodeURIComponent(from.toUpperCase())}&symbols=${encodeURIComponent(to.toUpperCase())}`,
+      `https://api.exchangeratesapi.io/latest?base=${encodeURIComponent(base.toUpperCase())}&symbols=${encodeURIComponent(to.toUpperCase())}`,
     ).then(async res => await res.json().catch(() => {}));
 
     if (!body) return this.bot.embed("❌ Error", "Failed to send the rates. Try again later.", msg, "error");
-    if (body.error && body.error !== undefined) return this.bot.embed("❌ Error", "No conversion rates found.", msg, "error");
+    if (body && body.error && body.error !== undefined) return this.bot.embed("❌ Error", "No conversion rates found.", msg, "error");
 
-    if (isNaN(amount)) {
-      this.bot.embed(
-        `💱 ${body.base} to ${to.toUpperCase()}`,
-        `${Object.keys(body.rates).map(k => `**${k}**: ${body.rates[k].toFixed(2)}`).join("\n")}`,
-        msg);
-    } else {
-      this.bot.embed(
-        "💱 Currency",
-        `**${amount}** ${body.base} ~ **${amount * body.rates[to.toUpperCase()].toFixed(2)}** ${to.toUpperCase()}`,
-        msg,
-      );
-    }
+    this.bot.embed(
+      "💱 Currency",
+      `**${from} ${body.base}** is approximately **${from * body.rates[to.toUpperCase()].toFixed(2)} ${to.toUpperCase()}**.`,
+      msg,
+    );
   }
 }
 
