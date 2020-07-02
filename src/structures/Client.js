b48d444148c08c3e7a9e1703d389e64c9658664f
Espi <espi@lesbian.codes>
2020-07-02 00:10:35 -0400

    src: move a few things out of global client

--- src/structures/Client.js
+++ src/structures/Client.js
@@ -5,9 +5,11 @@
 
 const { Client } = require("eris");
 const { r } = require("rethinkdb-ts");
-const Args = require("structures/Args");
-const config = require("root/config");
-const database = require("scripts/database");
+const Args = require("./Args");
+const config = require("../../config");
+const database = require("../scripts/database");
+const load = require("../scripts/loader");
+const statuses = require("../scripts/statuses");
 const sentry = require("@sentry/node");
 const startup = new Date();
 
@@ -16,8 +18,8 @@ class Hibiki extends Client {
    * Creates a bot instance
    *
    * @example
-   * const Hibiki = require("structures/Client");
-   * const config = require("root/config");
+   * const Hibiki = require("../structures/Client");
+   * const config = require("../config");
    * new Hibiki(token, options);
    *
    * @param {string} token Discord bot token from the config file
@@ -33,12 +35,10 @@ class Hibiki extends Client {
     this.db = r.db(config.rethink.db);
 
     this.args = new Args(this);
-    this.embed = require("utils/embed");
-    this.load = require("scripts/loader");
-    this.log = require("scripts/logger");
-    this.tag = require("utils/format").tag;
-    this.version = require("root/package").version;
-    this.statuses = require("scripts/statuses");
+    this.embed = require("../utils/embed");
+    this.log = require("../scripts/logger");
+    this.tag = require("../utils/format").tag;
+    this.version = require("../../package").version;
 
     this.commands = [];
     this.events = [];
@@ -52,9 +52,9 @@ class Hibiki extends Client {
 
   /** Runs when the bot is ready */
   readyListener() {
-    this.load.all(this);
+    load.all(this);
+    statuses.switch(this);
     try { sentry.init({ dsn: this.key.sentry }); } catch (err) { this.log.error(`Sentry failed to start: ${err}`); }
-    this.statuses.switch(this);
     this.log.info(`Logged in as ${this.user.username}#${this.user.discriminator} on ${this.guilds.size} servers`);
     this.log.info(`Startup took ${new Date(new Date() - startup).getSeconds()}.${new Date(new Date() - startup).getMilliseconds()} seconds`);
   }
