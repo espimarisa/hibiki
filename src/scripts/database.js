5d0d9b77ab81c9fcb82ab28081085ac83c1dcd98
Espi Marisa <hi@espi.me>
2020-07-29 00:08:36 -0400

    fix: database + docker checker

--- src/scripts/database.js
+++ src/scripts/database.js
@@ -5,35 +5,9 @@
  */
 
 const { r } = require("rethinkdb-ts");
-const { statSync, readFileSync } = require("fs");
+const docker = require("../utils/docker");
 const config = require("../../config");
 const log = require("./logger");
-let docker;
-
-// Hacky function to figure out if we're running in Docker
-function isDocker() {
-  function hasDockerEnv() {
-    try {
-      statSync("/.dockerenv");
-      return true;
-    } catch (err) {
-      return false;
-    }
-  }
-
-  function hasDockerCGroup() {
-    try {
-      return readFileSync("/proc/self/cgroup", "utf8").includes("docker");
-    } catch (err) {
-      return false;
-    }
-  }
-
-  if (docker === undefined) {
-    docker = hasDockerEnv() || hasDockerCGroup();
-    return docker;
-  }
-}
 
 /**
  * Starts RethinkDB with configured options
@@ -47,7 +21,7 @@ function isDocker() {
 module.exports.start = async function database() {
   await r.connectPool({
     db: config.rethink.db,
-    servers: isDocker() === true ? [{ host: "db" }] : [{ host: "localhost" }],
+    // servers: docker === true ? [{ host: "db" }] : [{ host: "localhost" }],
     password: config.rethink.password,
     port: config.rethink.port,
     user: config.rethink.user ? config.rethink.user : "admin",
