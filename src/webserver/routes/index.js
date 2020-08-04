4f7ec4a11586f8b544a036475915c8fdf430de8c
Espi <espi@lesbian.codes>
2020-08-04 19:59:32 -0400

    dash: strengthen securityPolicy settings and fix deauth

--- src/webserver/routes/index.js
+++ src/webserver/routes/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const format = require("../../utils/format");
 
 const router = express.Router();
 
 module.exports = (bot) => {
   router.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {
+      bot: bot,
+      format: format,
+    });
   });
 
   router.get("/login/", (req, res) => {
@@ -19,12 +23,16 @@ module.exports = (bot) => {
     res.redirect(301, `https://discordapp.com/oauth2/authorize?&client_id=${bot.user.id}&scope=bot&permissions=506850534`);
   });
 
-  router.get("/support/", (req, res) => {
+  router.get("/support/" || router.get("/discord"), (req, res) => {
     res.redirect(301, `https://discord.gg/${bot.config.support}`);
   });
 
-  router.get("/repo/", (req, res) => {
+  router.get("/github/" || router.get("/repo/"), (req, res) => {
     res.redirect(301, "https://github.com/smolespi/Hibiki");
+  });
+
+  router.get("/privacy/", (req, res) => {
+    res.redirect(301, "https://github.com/smolespi/Hibiki/blob/main/.github/PRIVACY_POLICY.md");
   });
 
   router.get("/vote/", (req, res) => {
