06e350236d1d638cb23aee47a2561868c8c020b7
Espi Marisa <hi@espi.me>
2020-08-09 00:29:02 -0400

    fix: message sniping

--- src/events/Snipe.js
+++ src/events/Snipe.js
@@ -11,16 +11,18 @@ class Snipe extends Event {
     if (!msg || !msg.channel || !msg.channel.guild || msg.author.bot) return;
     const guildcfg = await this.bot.db.table("guildcfg").get(msg.channel.guild.id).run();
     if (guildcfg) {
-      if (!guildcfg.snipingEnable) return;
+      if (guildcfg.snipingDisable) return;
       if ((!msg.attachments || msg.attachments[0] === undefined) && (!msg.content || !msg)) return;
       if (!guildcfg.snipingInvites && /(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li|list)|discord(app)?\.com\/invite)\/.+[a-z]/.test(msg.content)) {
         return;
       }
 
       let ignored;
-      guildcfg.snipingIgnore.forEach(c => {
-        if (c && msg.channel.id === c && msg.channel.guild.channels.has(c)) ignored = true;
-      });
+      if (guildcfg && guildcfg.snipingIgnore) {
+        guildcfg.snipingIgnore.forEach(c => {
+          if (c && msg.channel.id === c && msg.channel.guild.channels.has(c)) ignored = true;
+        });
+      }
 
       if (ignored) return;
     }
