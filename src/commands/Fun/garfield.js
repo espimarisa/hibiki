7b83bc177e241e21c4f9c1550eb12e5e31468e0b
Espi <espi@lesbian.codes>
2020-08-14 18:02:24 -0400

    feat: add support for daily garfield command (by @resolvedxd)
    Co-Authored-By: resolved <resolvedxd@gmail.com>

--- src/commands/Fun/garfield.js
+++ src/commands/Fun/garfield.js
@@ -9,13 +9,15 @@ class garfieldCommand extends Command {
     });
   }
 
-  run(msg) {
+  run(msg, args) {
     // Gets a random comic
-    function GarfieldRandom() {
+    function GarfieldRandom(latest = false) {
       const days = 24 * 60 * 60 * 1000;
       const today = new Date();
       const start = new Date("1978/06/19");
-      const date = new Date(start.getTime() + Math.random() * (today.getTime() - start));
+      let date;
+      if (latest) date = new Date();
+      else date = new Date(start.getTime() + Math.random() * (today.getTime() - start));
 
       // Gets total amount of comics
       const total = Math.round(Math.abs((start.getTime() - today.getTime()) / (days)));
@@ -24,11 +26,11 @@ class garfieldCommand extends Command {
       function pad(n) { return n < 10 ? `0${n}` : n; }
       const archive = "https://d1ejxu6vysztl5.cloudfront.net/comics/garfield/";
       const url = `${archive + date.getFullYear()}/${date.getFullYear()}-${pad(date.getMonth())}-${pad(date.getDate())}.gif`;
-      const garfield = [url, date.getFullYear(), pad(date.getMonth()), pad(date.getDate()), total];
+      const garfield = [url, date.getFullYear(), pad(date.getMonth() + 1), pad(date.getDate() + 1), total];
       return garfield;
     }
 
-    const garfield = GarfieldRandom();
+    const garfield = GarfieldRandom(args.map(a => a.toLowerCase()).includes("today"));
     if (!garfield[0]) return this.bot.embed("❌ Error", "Couldn't send the comic. Try again later.", msg, "error");
 
     msg.channel.createMessage({
