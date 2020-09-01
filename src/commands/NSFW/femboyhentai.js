7f97ecef81af3d25e43bb019f5e94793fd71772d
Espi Marisa <hi@espi.me>
2020-09-01 18:30:49 -0400

    feat: add lots more NSFW commands

---
+++ src/commands/NSFW/femboyhentai.js
@@ -0,0 +1,34 @@
+const Command = require("../../structures/Command");
+const fetch = require("node-fetch");
+
+class femboyhentaiCommand extends Command {
+  constructor(...args) {
+    super(...args, {
+      aliases: ["lewdfemboy", "lewdtrap"],
+      description: "Sends a NSFW femboy image.",
+      nsfw: true,
+      cooldown: 3,
+    });
+  }
+
+  async run(msg) {
+    const body = await fetch("https://nekos.life/api/v2/img/trap").then(res => res.json().catch(() => {}));
+    if (!body || !body.url) return this.bot.embed("❌ Error", "Couldn't send the image. Try again later.", msg, "error");
+
+    msg.channel.createMessage({
+      embed: {
+        title: "🔞 Femboy Hentai",
+        color: this.bot.embed.color("general"),
+        image: {
+          url: body.url,
+        },
+        footer: {
+          text: `Ran by ${this.bot.tag(msg.author)} | Powered by nekos.life`,
+          icon_url: msg.author.dynamicAvatarURL(),
+        },
+      },
+    });
+  }
+}
+
+module.exports = femboyhentaiCommand;
---
+++ src/commands/NSFW/thighs.js
@@ -0,0 +1,34 @@
+const Command = require("../../structures/Command");
+const fetch = require("node-fetch");
+
+class thighsCommand extends Command {
+  constructor(...args) {
+    super(...args, {
+      aliases: ["thigh"],
+      description: "Sends a random NSFW IRL thigh picture.",
+      nsfw: true,
+      cooldown: 3,
+    });
+  }
+
+  async run(msg) {
+    const body = await fetch("https://nekobot.xyz/api/image?type=thigh").then(res => res.json().catch(() => {}));
+    if (!body || !body.message) return this.bot.embed("❌ Error", "Couldn't send the image. Try again later.", msg, "error");
+
+    msg.channel.createMessage({
+      embed: {
+        title: "🔞 Thighs",
+        color: this.bot.embed.color("general"),
+        image: {
+          url: body.message,
+        },
+        footer: {
+          text: `Ran by ${this.bot.tag(msg.author)} | Powered by api.nekobot.xyz`,
+          icon_url: msg.author.dynamicAvatarURL(),
+        },
+      },
+    });
+  }
+}
+
+module.exports = thighsCommand;
