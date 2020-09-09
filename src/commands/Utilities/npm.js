be73359d4b9ddf7c614544c00a9c424ea9b6d94f
Espi Marisa <hi@espi.me>
2020-09-09 18:48:13 -0400

    fix: various npm bugs

--- src/commands/Utilities/npm.js
+++ src/commands/Utilities/npm.js
@@ -15,17 +15,16 @@ class npmCommand extends Command {
   async run(msg, args) {
     const body = await fetch(`https://registry.npmjs.com/${encodeURIComponent(args.join(" ").toLowerCase())}`)
       .then(res => res.json().catch(() => {}));
+
     if (body.error || !body["dist-tags"]) return this.bot.embed("❌ Error", "Package not found.", msg, "error");
     const pkg = body.versions[body["dist-tags"].latest];
 
     // Embed fields
     const fields = [];
-    if (pkg.keywords && pkg.keywords.length > 0) {
-      fields.push({
-        name: "Keywords",
-        value: `${pkg.keywords.map(k => `\`${k}\``).join(", ")}`,
-      });
-    }
+    if (pkg.keywords && pkg.keywords.length > 0) fields.push({
+      name: "Keywords",
+      value: `${pkg.keywords.map(k => `\`${k}\``).join(", ")}`,
+    });
 
     fields.push({
       name: "Link",
@@ -39,31 +38,25 @@ class npmCommand extends Command {
     });
 
     if (body.license && !body.license.type) fields.push({ name: "License", value: `${body.license}`, inline: true });
-    if (body.license.type) fields.push({ name: "License", value: `${body.license.type}`, inline: true });
+    if (body.license && body.license.type) fields.push({ name: "License", value: `${body.license.type}`, inline: true });
 
-    if (pkg.maintainers.length) {
-      fields.push({
-        name: "Maintainers",
-        value: pkg.maintainers.map(m => `\`${m.name}\``).join(", "),
-        inline: true,
-      });
-    }
+    if (pkg.maintainers && pkg.maintainers.length) fields.push({
+      name: "Maintainers",
+      value: pkg.maintainers.map(m => `\`${m.name}\``).join(", "),
+      inline: true,
+    });
 
-    if (body.time.created) {
-      fields.push({
-        name: "Created",
-        value: `${format.date(body.time.created)}`,
-        inline: true,
-      });
-    }
+    if (body.time && body.time.created) fields.push({
+      name: "Created",
+      value: `${format.date(body.time.created)}`,
+      inline: true,
+    });
 
-    if (body.time.modified) {
-      fields.push({
-        name: "Updated",
-        value: `${format.date(body.time.modified)}`,
-        inline: true,
-      });
-    }
+    if (body.time && body.time.modified) fields.push({
+      name: "Updated",
+      value: `${format.date(body.time.modified)}`,
+      inline: true,
+    });
 
     msg.channel.createMessage({
       embed: {
