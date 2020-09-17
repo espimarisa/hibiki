77b1c5b3e0ca1a0642116438a50769a927496cc3
Espi <espi@lesbian.codes>
2020-09-17 00:08:51 -0400

    fix: various nhentai bugs

--- src/commands/NSFW/nhentai.js
+++ src/commands/NSFW/nhentai.js
@@ -1,8 +1,6 @@
-// Todo: implement reader support that @resolvedxd made
 const Command = require("../../structures/Command");
 const format = require("../../utils/format");
 const fetch = require("node-fetch");
-const maxTags = 8;
 
 class nhentaiCommand extends Command {
   constructor(...args) {
@@ -20,6 +18,7 @@ class nhentaiCommand extends Command {
     // Looks for a valid ID
     if (args.length && /\d{1,6}/.test(args.join(" "))) {
       const body = await fetch(`https://nhentai.net/api/gallery/${encodeURIComponent(args.join(" "))}`).then(res => res.json().catch(() => {}));
+      if (!body) return this.bot.embed("❌ Error", "Doujin not found.", msg, "error");
       book = body;
     } else if (args.length) {
       // Searches for a doujin
@@ -31,42 +30,58 @@ class nhentaiCommand extends Command {
     } else {
       // Gets a random book
       const body = await fetch(`https://nhentai.net/api/gallery/${Math.floor(Math.random() * 310000)}`).then(res => res.json().catch(() => {}));
+      if (!body) return this.bot.embed("❌ Error", "No doujin found. Try again later.", msg, "error");
       book = body;
     }
 
     // Sorts the tags
-    book.tags = book.tags.sort((a, b) => {
-      if (a.name < b.name) return -1;
-      if (a.name > b.name) return 1;
-      return 0;
+    if (book && book.tags && book.tags.length) {
+      book.tags = book.tags.sort((a, b) => {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      });
+    }
+
+    const fields = [];
+    fields.push({
+      name: "ID",
+      value: book.id,
+      inline: true,
     });
+
+    fields.push({
+      name: "Total Pages",
+      value: book.num_pages,
+      inline: true,
+    });
+
+    if (book.num_favorits && book.num_favorites.length) fields.push({
+      name: "Favorites",
+      value: book.num_favorites,
+      inline: true,
+    });
+
+    if (book.upload_date) fields.push({
+      name: "Uploaded",
+      value: format.date(book.upload_date * 1000),
+      inline: false,
+    });
+
+    if (book.tags && book.tags.length) {
+      fields.push({
+        name: "Tags",
+        value: `${book.tags.slice(0, 10).map(t => `\`${t.name}\``).join(",")}${book.tags.length > 10 ? ` and ${book.tags.length - 10} more` : ""}`,
+        inline: false,
+      });
+    }
 
     msg.channel.createMessage({
       embed: {
         color: 0xEC2854,
-        fields: [{
-          name: "ID",
-          value: book.id,
-          inline: true,
-        }, {
-          name: "Pages",
-          value: book.num_pages,
-          inline: true,
-        }, {
-          name: "Favorites",
-          value: book.num_favorites,
-          inline: true,
-        }, {
-          name: "Uploaded",
-          value: format.date(book.upload_date * 1000),
-          inline: false,
-        }, {
-          name: "Tags",
-          value: `${book.tags.slice(0, maxTags).map(t => `\`${t.name}\``).join(",")}${book.tags.length > maxTags ? ` and ${book.tags.length - maxTags} more` : ""}`,
-          inline: false,
-        }],
+        fields: fields,
         author: {
-          name: book.title.pretty || book.title.english,
+          name: book.title.pretty || book.title.english || "Unknown Title",
           icon_url: "https://i.imgur.com/jboGQa0.png",
           url: `https://nhentai.net/g/${book.id}/`,
         },
