88191eb5a458e0065900255745365173ba935746
Espi <espi@lesbian.codes>
2020-09-16 21:18:43 -0400

    refactor: allow member argtype to find member by nickname in non-strict mode

--- src/utils/argtypes/member.js
+++ src/utils/argtypes/member.js
@@ -1,6 +1,7 @@
 module.exports = [function member(a, msg, flag) {
   const member = msg.channel.guild.members.find(m => flag !== "strict" ?
-    m.username.toLowerCase() === a || m.id === a || a.startsWith(`<@!${m.id}>`) || a.startsWith(`<@${m.id}>`) :
+    m.username.toLowerCase() === a || m.id === a || a.startsWith(`<@!${m.id}>`) || a.startsWith(`<@${m.id}>`) ||
+    m.nick && m.nick.toLowerCase() === a :
     m.username.startsWith(a) || m.id === a || a.startsWith(`<@!${m.id}>`) || a.startsWith(`<@${m.id}>`));
   if ((!a || !member) && flag === "fallback") return msg.channel.guild.members.get(msg.author.id);
   if (!flag && member && member.id === msg.author.id) return;
