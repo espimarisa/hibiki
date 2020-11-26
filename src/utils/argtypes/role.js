cc4e8217fe48f0b90c2cc881118524a8ad3fecb2
Espi <contact@espi.me>
2020-11-25 20:23:09 -0500

    chore: clean out src directory

--- src/utils/argtypes/role.js
+++
@@ -1,4 +0,0 @@
-module.exports = [function role(a, msg) {
-  const role = msg.channel.guild.roles.find(r => r.id === a || a.startsWith(`<@&${r.id}>`) || r.name.toLowerCase().startsWith(a.toLowerCase()));
-  return role;
-}];
