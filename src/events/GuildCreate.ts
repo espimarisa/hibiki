6dc30a9806b9bc6e34fc65d5ed1306a88a43b11d
Espi <contact@espi.me>
2020-12-15 21:07:09 -0500

    feat: prevent bot from being added to blacklisted guilds

--- src/events/GuildCreate.ts
+++ src/events/GuildCreate.ts
@@ -1,12 +1,33 @@
 import { Guild } from "eris";
 import { HibikiClient } from "classes/Client";
 import { Event } from "../classes/Event";
+import axios from "axios";
 
 class GuildCreateEvent extends Event {
   events = ["guildCreate"];
 
   async run(guild: Guild, bot: HibikiClient) {
+    // Checks to see if the bot was added to a blacklisted guild
+    const blacklist = await bot.db.getBlacklistedGuild(guild.id);
+    if (blacklist) {
+      bot.log.warn(`Added to blacklisted guild, leaving: ${guild.name} (${guild.id})`);
+      return guild.leave();
+    }
+
     bot.log.info(`Added to guild: ${guild.name} (${guild.id})`);
+
+    // DMs the owner the welcome message
+    const owner = bot.users.get(guild.ownerID);
+    if (owner && owner.id) {
+      const ownerDM = await owner.getDMChannel();
+      ownerDM.createMessage({
+        embed: {
+          title: "hello",
+        },
+      });
+    }
+
+    // Updates stats on bot listing websites if applicable
   }
 }
 
