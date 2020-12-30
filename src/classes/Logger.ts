e1c1ce75e6e20352b9d7304f9e17cf5165af1148
Espi <contact@espi.me>
2020-12-30 01:05:01 -0500

    feat: logging handler and a few loggers, setup command, some incomplete stuff
    Co-Authored-By: resolved <resolvedxd@gmail.com>

--- src/classes/Logger.ts
+++ src/classes/Logger.ts
@@ -3,4 +3,35 @@
  * @description Base class for loggers
  */
 
-export abstract class Logger {}
+import { Guild, TextChannel } from "eris";
+import { Event } from "./Event";
+
+export abstract class Logger extends Event {
+  async getChannel(channel: TextChannel | Guild, type: string, event: string) {
+    if (type !== "eventLogging" && type !== "memberLogging" && type !== "messageLogging") type = "modLogging";
+    let guild;
+    if (channel.constructor === Guild) guild = channel;
+    // @ts-expect-error
+    else guild = channel.guild;
+
+    const guildconfig = await this.bot.db.getGuildConfig(guild.id);
+    if (!guildconfig) return null;
+
+    // Ignores channels set to be ignored
+    if (channel.constructor === TextChannel && guildconfig.ignoredLoggingChannels && guildconfig.ignoredLoggingChannels.includes(channel))
+      return null;
+    if (guildconfig.disabledEvents && guildconfig.disabledEvents.includes(event)) return null;
+
+    let channelID = type ? guildconfig[type] : guildconfig.loggingChannel;
+
+    if (channel.constructor === Guild || channel.id !== channelID) {
+      if (!guild.channels.get(channelID)) {
+        delete guildconfig[type || "loggingChannel"];
+        this.bot.db.db.table("guildconfig").update(guildconfig);
+        channelID = undefined;
+      }
+    }
+
+    return channelID;
+  }
+}
