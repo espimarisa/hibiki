a100a3a4ab2858d4144f2c8fbf6514450dc25b6d
Espi <espi@riseup.net>
2021-01-15 00:03:12 -0500

    cmds: add more image commands

---
+++ src/commands/image/awoo.ts
@@ -0,0 +1,42 @@
+import type { Message, TextChannel } from "eris";
+import { Command } from "../../classes/Command";
+import config from "../../../config.json";
+import axios from "axios";
+
+export class AwooCommand extends Command {
+  aliases = ["momiji"];
+  description = "Sends a picture of Momiji Inubashiri (awoo!).";
+  cooldown = 3000;
+
+  async run(msg: Message<TextChannel>) {
+    const body = await axios
+      .get("https://api.weeb.sh/images/random?type=awoo", {
+        headers: {
+          "Authorization": `Wolke ${config.keys.weebsh}`,
+          "User-Agent": "hibiki",
+        },
+      })
+      .catch(() => {});
+
+    let image: string;
+    if (!body || !body?.data?.url) image = "https://cdn.weeb.sh/images/H1Kvalk9W.png";
+    else image = body.data.url;
+
+    msg.channel.createMessage({
+      embed: {
+        description: `🐺 ${msg.string("image.AWOO")}`,
+        color: msg.convertHex("general"),
+        image: {
+          url: image,
+        },
+        footer: {
+          text: msg.string("global.RAN_BY", {
+            author: msg.tagUser(msg.author),
+            poweredBy: "weeb.sh",
+          }),
+          icon_url: msg.author.dynamicAvatarURL(),
+        },
+      },
+    });
+  }
+}
---
+++ src/commands/image/engineeredcatgirls.ts
@@ -0,0 +1,38 @@
+import type { Message, TextChannel } from "eris";
+import { Command } from "../../classes/Command";
+import { resError } from "../../utils/exception";
+import axios from "axios";
+
+export class EngineeredcatgirlsCommand extends Command {
+  aliases = ["gecg", "gecatgirl", "gecatgirls", "geneticallyengineeredcatgirls"];
+  description = "Sends a genetically engineered catgirl meme.";
+  cooldown = 3000;
+  allowdms = true;
+
+  async run(msg: Message<TextChannel>) {
+    const body = await axios.get("https://nekos.life/api/v2/img/gecg").catch((err) => {
+      resError(err);
+    });
+
+    if (!body || !body.data?.url) {
+      return msg.createEmbed(msg.string("global.ERROR"), msg.string("global.RESERROR_IMAGE"), "error");
+    }
+
+    msg.channel.createMessage({
+      embed: {
+        title: `🐱 ${msg.string("image.GENETICALLY_ENGINEERED_CATGIRLS")}`,
+        color: msg.convertHex("general"),
+        image: {
+          url: body.data.url,
+        },
+        footer: {
+          text: msg.string("global.RAN_BY", {
+            author: msg.tagUser(msg.author),
+            poweredBy: "nekos.life",
+          }),
+          icon_url: msg.author.dynamicAvatarURL(),
+        },
+      },
+    });
+  }
+}
