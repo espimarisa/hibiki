05fecb14e70c6741a53b1f33dc6c3670acdd527e
Espi <espi@riseup.net>
2021-01-20 21:09:47 -0500

    fix: don't load commands if they're missing requiredkeys
    Co-authored-by: resolved <resolvedxd@gmail.com>

--- src/helpers/loader.ts
+++ src/helpers/loader.ts
@@ -5,17 +5,19 @@
  */
 
 import type { HibikiClient } from "../classes/Client";
+import type { Command } from "../classes/Command";
 import type { Event } from "../classes/Event";
 import { readdir } from "fs";
-import { join } from "path";
+import path from "path";
+import config from "../../config.json";
 
-const COMMANDS_DIRECTORY = join(__dirname, "../commands");
-const EVENTS_DIRECTORY = join(__dirname, "../events");
-const LOGGERS_DIRECTORY = join(__dirname, "../loggers");
+const COMMANDS_DIRECTORY = path.join(__dirname, "../commands");
+const EVENTS_DIRECTORY = path.join(__dirname, "../events");
+const LOGGERS_DIRECTORY = path.join(__dirname, "../loggers");
 const fileTypes = /\.(js|ts)$/i;
 const loadedCommands = {};
 
-/** Loads all commands */
+// Loads all commands
 function loadCommands(path: string, bot: HibikiClient) {
   readdir(path, { withFileTypes: true }, (ioerr, files) => {
     if (ioerr) return;
@@ -40,9 +42,18 @@ function loadCommands(path: string, bot: HibikiClient) {
 
       if (!command) return;
 
-      // Pushes the commands
+      // Gets the command to load
       const splitPath = path.split("/");
-      bot.commands.push(new command(bot, file.name.split(fileTypes)[0], splitPath[splitPath.length - 1]));
+      const cmd = new command(bot, file.name.split(fileTypes)[0], splitPath[splitPath.length - 1]) as Command;
+
+      // Don't load commands missing requiredkeys
+      let cmdMissingKeys = false;
+      if (!cmd.requiredkeys.every((k: string) => (Object.keys(config.keys).includes(k) && config.keys[k]) || config.keys.botlists[k])) {
+        cmdMissingKeys = true;
+      }
+
+      // Pushes the commands
+      if (!cmdMissingKeys) bot.commands.push(cmd);
 
       if (i === files.length - 1) {
         loadedCommands[path] = true;
@@ -52,7 +63,7 @@ function loadCommands(path: string, bot: HibikiClient) {
   });
 }
 
-/** Loads and runs events/loggers */
+// Loads and runs events and loggers
 function loadEvents(path: string, bot: HibikiClient, logger = false) {
   // Subscribes to and runs events/loggers
   function subscribeEvents(array: Event[] = []) {
@@ -93,7 +104,7 @@ function loadEvents(path: string, bot: HibikiClient, logger = false) {
   });
 }
 
-/** Loads all items */
+// Loads all items
 export async function loadItems(bot: HibikiClient) {
   loadCommands(COMMANDS_DIRECTORY, bot);
   loadEvents(EVENTS_DIRECTORY, bot);
