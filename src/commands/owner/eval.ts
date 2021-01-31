8cbda012831e31926ffa726ac692eb9f5131aee2
resolved <resolvedxd@gmail.com>
2021-01-31 01:57:54 -0500

    fix: eval not filtering keys/tokens

--- src/commands/owner/eval.ts
+++ src/commands/owner/eval.ts
@@ -4,6 +4,20 @@ import { Command } from "../../classes/Command";
 import config from "../../../config.json";
 import axios from "axios";
 
+// Hides API keys and the bot token
+const tokens: string[] = [config.token];
+Object.values(config.keys).forEach((key) => {
+  if (typeof key === "object")
+    Object.values(key).forEach((key2: any) => {
+      if (key2?.auth) tokens.push(key2.auth);
+      else if (key2.length) tokens.push(key2);
+    });
+  else if (key.length) tokens.push(key);
+});
+
+// Tokens to hide
+const tokenRegex = new RegExp(tokens.join("|"), "g");
+
 export class EvalCommand extends Command {
   description = "Evaluates some code.";
   args = "[code:string]";
@@ -27,8 +41,7 @@ export class EvalCommand extends Command {
       } else {
         msg.createEmbed(
           msg.string("global.SUCCESS"),
-          // todo: replace anything in config.json keys or token
-          `\`\`\`js\n${evalstring.replace(config.token, "Bot token hidden.")}\n\`\`\``,
+          `\`\`\`js\n${evalstring.replace(tokenRegex, msg.string("owner.TOKEN_HIDDEN"))}\n\`\`\``,
           "success",
         );
       }
