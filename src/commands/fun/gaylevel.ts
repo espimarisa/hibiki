23f5593b705d812003cca3f18eeb313b098a6913
resolved <resolvedxd@gmail.com>
2021-01-31 01:42:24 -0500

    feat: customizable gayLevel

--- src/commands/fun/gaylevel.ts
+++ src/commands/fun/gaylevel.ts
@@ -8,9 +8,13 @@ export class GaylevelCommand extends Command {
 
   async run(msg: Message<TextChannel>, pargs: ParsedArgs[]) {
     const member = pargs[0].value as Member;
-    const random = Math.floor(Math.random() * 99) + 1;
+    const usercfg = await this.bot.db.getUserConfig(member.id);
+    const gayLevel = usercfg?.gayLevel || Math.round(Math.random() * 100);
 
     // TODO: allow people to set gaylevel in configuser
-    msg.createEmbed(`🏳️‍🌈 ${msg.string("fun.GAYLEVEL")}`, msg.string("fun.GAYLEVEL_LEVEL", { member: member.user.username, level: random }));
+    msg.createEmbed(
+      `🏳️‍🌈 ${msg.string("fun.GAYLEVEL")}`,
+      msg.string("fun.GAYLEVEL_LEVEL", { member: member.user.username, level: gayLevel }),
+    );
   }
 }
