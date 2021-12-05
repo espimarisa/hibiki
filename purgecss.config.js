1ad963419f7ea9fc3879ba1bb963b15a3a1776e0
Espi <espi@riseup.net>
2021-12-05 01:58:04 -0500

    chore: remove old code; add most base config files; initialise the project rewrite

--- purgecss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ["./src/webserver/**/**/*.{liquid,js}", "./node_modules/bulmaselect/dist/index.js"],
-  css: ["./src/webserver/**/**/*.css"],
-  fontFace: true,
-  variables: true,
-  keyframes: true,
-};
