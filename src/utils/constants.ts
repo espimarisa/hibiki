0e2c078f604cf8774413b3978914fdad2682dcb0
Espi <espi@riseup.net>
2022-01-11 02:36:03 -0500

    fix: those are not java web tokens i am tired

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -10,7 +10,7 @@ export const moduleFiletypeRegex = /\.(js|ts)$/i;
 // A regex for valid HTTP(s) addresses
 export const httpRegex = /https?:\/\/(?:www\.)?([\w#%+.:=@~-]{1,256})\.[\d()A-Za-z]{1,6}\b([\w#%&()+./:=?@~-]*)/;
 
-// A regex for valid Java Web Tokens
+// A regex for valid JSON Web Tokens
 export const jwtRegex = /^[\w=-]+\.[\w=-]+\.?[\w+./=-]*$/;
 
 // A regex for valid colour codes
