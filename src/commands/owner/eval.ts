f83f81e8bbdd317158f01cb6e016d2133b9940cf
Flleeppyy <flleeppyybusiness@gmail.com>
2022-04-06 18:43:08 -0700

    fix: validate and format eval return strings

--- src/commands/owner/eval.ts
+++ src/commands/owner/eval.ts
@@ -1,18 +1,19 @@
 import type { Message } from "discord.js";
 import { HibikiCommand } from "../../classes/Command";
+import { clean } from "../../utils/strings";
 import util from "node:util";
 
 export class EvalCommand extends HibikiCommand {
   description = "Evaluates some code.";
-  // messageOnly = true;
+  messageOnly = true;
   ownerOnly = true;
 
   public async runWithMessage(msg: Message, args: string[]) {
     try {
       const evaluated = await eval(`(async () => {\n${args.join(" ")}\n})()`);
       const evalstring = typeof evaluated === "string" ? evaluated : util.inspect(evaluated);
-      console.log(evalstring);
-      await msg.reply(evalstring);
+      const returning = `\`\`\`js\n${clean(evalstring)}\n\`\`\``;
+      await msg.reply(returning);
     } catch (error) {
       await msg.reply((error as Error).message);
       console.error(error);
