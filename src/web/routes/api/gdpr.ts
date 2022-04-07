85edfbd0b5af527ca7d80c373d5ce43a843b0c18
Flleeppyy <flleeppyybusiness@gmail.com>
2022-04-06 18:48:26 -0700

    feat: switching from internal web api to just a web api that we send requests to

---
+++ src/web/routes/api/gdpr.ts
@@ -0,0 +1,10 @@
+import type { FastifyGenericRouteOptions, FastifyNextFunction, FastifyServer } from "web/server";
+
+export function gdprRoutes(app: FastifyServer, options: FastifyGenericRouteOptions, next: FastifyNextFunction) {
+  
+  app.get("/api/gdpr", async (req, res) => {
+    
+  });
+
+  next();
+}
