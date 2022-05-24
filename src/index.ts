33f09b839a69a076702c1f529f7ac62a130ea579
Espi Marisa <hi@espi.me>
2022-05-23 22:40:29 -0400

    chore: remove any old work; begin ESM rewrite + migration
    I promise I'll finish this...

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,5 @@
-/**
- * @file Index
- * @description Creates a new sharding manager and webserver
- * @module index
- */
-
 import config from "../config.js";
-import { HibikiShardingManager } from "./classes/Sharder.js";
-import { validateConfig } from "./utils/validator.js";
-import { startWebserver } from "./web/server.js";
-import path from "node:path";
-import url from "node:url";
+import { HibikiClient } from "./classes/HibikiClient.js";
 
-// Checks to see if the config is valid
-validateConfig(config);
-
-// Finds the index file
-const HIBIKI_INDEX_FILE = path.join(
-  path.dirname(url.fileURLToPath(import.meta.url)),
-  `hibiki.${process.env.NODE_ENV === "development" ? "ts" : "js"}`,
-);
-
-// Creates and spawns the sharding manager
-const manager = new HibikiShardingManager(HIBIKI_INDEX_FILE, config.hibiki.token, "auto");
-manager.spawn();
-
-// Starts the webserver
-// shouldn't we not pass an entire class thru this, and instead run 2 functions?
-// or join the 2, webserver stuff is a pain
-
-if (process.uptime() < 30) {
-  startWebserver();
-}
+const bot = new HibikiClient(config);
+bot.init();
