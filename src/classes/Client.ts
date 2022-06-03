446b91d98ed943ceaa82fb7c6cdc8ed8d18d4e79
Espi <espi@riseup.net>
2022-06-03 15:32:05 -0400

    feat: prisma database support; ESM skeleton

--- src/classes/Client.ts
+++ src/classes/Client.ts
@@ -3,10 +3,18 @@
  * @description Connects to Discord and handles global functions
  */
 
+import { logger } from "../utils/logger.js";
+import { DatabaseManager } from "./Database.js";
 import { Client, Intents } from "discord.js";
 
 export class HibikiClient extends Client {
   readonly config: HibikiConfig;
+
+  // Hibiki's current version, defined in package.json
+  readonly version: string = process.env.npm_package_version ?? "develop";
+
+  // A Prisma + Hibiki Database Manager
+  readonly db: DatabaseManager = new DatabaseManager();
 
   constructor(config: HibikiConfig) {
     super({
@@ -19,9 +27,13 @@ export class HibikiClient extends Client {
     this.once("ready", async () => this.init());
   }
 
-  public init() {
+  /**
+   * Initialises Hibiki
+   */
+
+  public async init() {
     this.login(this.config.hibiki.token).then(async () => {
-      console.log(`Signed in as ${this.user?.tag}`);
+      logger.info(`Logged in as ${this.user?.tag} in ${this.guilds.cache.size} guilds on shard #${this.shard?.ids[0]}}`);
     });
   }
 }
