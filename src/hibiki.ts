5f3aaf4624bd75d6c8cd425f77a986058cd60459
Espi <espi@riseup.net>
2022-06-28 22:08:07 -0400

    fix: weird jankass double sharding thing

--- src/hibiki.ts
+++ src/hibiki.ts
@@ -8,5 +8,4 @@ import config from "../config.js";
 import { HibikiClient } from "./classes/Client.js";
 
 // Spawns a new instance of Hibiki
-const bot = new HibikiClient(config);
-bot.login(config.hibiki.token);
+new HibikiClient(config).init();
