39e7a62ca2ecd979d4881d036a621bba6015a367
Espi <espi@riseup.net>
2022-07-03 15:41:34 -0400

    chore: fix broken discordjs collection type

--- src/commands/general/ping.ts
+++ src/commands/general/ping.ts
@@ -2,14 +2,15 @@ import type { CommandInteraction } from "discord.js";
 import { HibikiCommand } from "../../classes/Command.js";
 
 export class PingCommand extends HibikiCommand {
-  description = "Checks the current status of the bot - test owowowowowowowowoo.";
+  description = "Checks the current status of the bot and returns current latency.";
 
   public async runWithInteraction(interaction: CommandInteraction) {
     await interaction.reply({
       embeds: [
         {
-          title: "ping",
-          description: this.bot.ws.ping.toString(),
+          title: interaction.getString("general.COMMAND_PING_PONG"),
+          description: interaction.getString("general.COMMAND_PING_DESCRIPTION", { latency: this.bot.ws.ping }),
+          color: this.bot.config.colours.primary,
         },
       ],
     });
--- src/typings/discord.js.d.ts
+++ src/typings/discord.js.d.ts
@@ -8,10 +8,10 @@ import type { getString } from "./locales.js";
 
 declare module "discord.js" {
   declare interface CommandInteraction {
-    getString?: getString;
+    getString: getString;
   }
 
   declare interface Message {
-    getString?: getString;
+    getString: getString;
   }
 }
