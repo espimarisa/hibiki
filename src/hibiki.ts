c0e2ccc964cab3b7bb4236733c4492b13f2b7d85
Espi Marisa <hi@espi.me>
2023-04-21 00:34:07 -0500

    feat: add sentry/glitchtip support

--- src/hibiki.ts
+++ src/hibiki.ts
@@ -6,11 +6,29 @@
 
 import { HibikiClient } from "./classes/Client.js";
 import { env } from "./utils/env.js";
+import { logger } from "./utils/logger.js";
+import * as Sentry from "@sentry/node";
 import { getInfo } from "discord-hybrid-sharding";
+import util from "node:util";
 
+// Tries to initialize Sentry
+if (env.SENTRY_DSN) {
+  try {
+    Sentry.init({
+      dsn: env.SENTRY_DSN,
+      environment: process.env.NODE_ENV,
+      release: process.env.npm_package_version,
+    });
+
+    logger.info(`Sentry connected to DSN ${env.SENTRY_DSN}`);
+  } catch (error) {
+    logger.error(`Failed to initialize sentry: ${util.inspect(error)}`);
+  }
+}
 // Creates the initial instance of Hibiki
 new HibikiClient(env.BOT_TOKEN, {
   defaultImageSize: 256,
+  restMode: true,
   gateway: {
     firstShardID: getInfo().FIRST_SHARD_ID,
     lastShardID: getInfo().LAST_SHARD_ID,
@@ -20,9 +38,6 @@ new HibikiClient(env.BOT_TOKEN, {
     disableEvents: {
       // Events to opt out of for some performance boosts
       TYPING_START: true,
-      USER_NOTE_UPDATE: true,
-      RELATIONSHIP_ADD: true,
-      RELATIONSHIP_REMOVE: true,
     },
   },
 });
