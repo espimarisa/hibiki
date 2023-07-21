867440a14866ce74c0ebeef26aac6644ac9807fb
Espi Marisa <hi@espi.me>
2023-07-21 01:08:15 -0500

    chore: cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,13 @@ import { sanitizedEnv } from "./utils/env.js";
 import path from "node:path";
 import url from "node:url";
 
+// Loads i18n
 import "./utils/i18n.js";
 
 // Searches for the root Hibiki file
 const pathDirname = path.dirname(url.fileURLToPath(import.meta.url));
 const HIBIKI_INDEX_FILE = `${pathDirname}/hibiki.${sanitizedEnv.isProduction ? "js" : "ts"}`;
 
-// Loads i18n
-
-// Creates a new sharding manager/
+// Creates a new sharding manager
 const manager = new HibikiShardingManager(HIBIKI_INDEX_FILE, sanitizedEnv.TOKEN, "auto");
 manager.spawn();
