838affb8ffd2dd9ad22389ea9217261fd42f8a17
Espi Marisa <hi@espi.me>
2023-07-21 00:56:27 -0500

    feat: add a new locale system using i18n-next

---
+++ src/typings/i18next.d.ts
@@ -0,0 +1,17 @@
+import "i18next";
+
+// Import all namespaces
+import bot from "../../locales/en/bot.json";
+import web from "../../locales/en/web.json";
+
+declare module "i18next" {
+  // Extend CustomTypeOptions
+  interface CustomTypeOptions {
+    defaultNS: "bot";
+
+    resources: {
+      bot: typeof bot;
+      web: typeof web;
+    };
+  }
+}
