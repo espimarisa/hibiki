65f54b762b3b9b715ea9464f5e586cc9495bcee3
Espi <espi@riseup.net>
2023-07-21 00:56:27 -0500

    feat: add a new locale system using i18n-next

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,9 +1,10 @@
 import type { ChatInputCommandInteraction } from "discord.js";
 import { HibikiCommand } from "../classes/Command.js";
-import { HibikiColors } from "../utils/constants.js";
 
 export class HibikiPingCommand extends HibikiCommand {
   description = "Checks the current status and latency.";
 
-  public async runWithInteraction(interaction: ChatInputCommandInteraction) {}
+  public async runWithInteraction(interaction: ChatInputCommandInteraction) {
+    await interaction.followUp("This is a test :3");
+  }
 }
