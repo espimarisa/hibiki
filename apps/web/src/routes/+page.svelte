42b3cf8472cfd88b97888c52eef8a92e0661f552
Espi <espi@riseup.net>
2023-07-23 02:09:21 -0500

    feat(web): add very WIP landing page, implement redirect routes

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,1 +1,38 @@
-<h1>Hello Hibiki web!</h1>
+<nav id="navbar">
+  <a href="/invite">Invite</a>
+  <a href="/support">Support</a>
+  <a href="/donate">Donate</a>
+  <a href="/github">GitHub</a>
+  <div class="navbar-right">
+    <a href="/dashboard">Login</a>
+  </div>
+</nav>
+
+<div class="large-hero">
+  <span class="title">Hibiki</span>
+  <br />
+  <span class="subtitle">A Discord bot jam-packed with useful utilities for everyone.</span>
+  <br />
+  <button class="button">
+    <a href="/invite">Invite to server</a>
+  </button>
+  <br />
+</div>
+
+<div class="small-hero">
+  <span class="small-title">Fully free, forever.</span>
+  <br />
+  <span class="small-subtitle">
+    We'll never lock features behind a paywall, force you to donate, or make you buy some sketchy blockchain powered thing in order to use
+    all of Hibiki's features.
+  </span>
+  <br />
+  <br />
+  <span class="small-subtitle">
+    Hibiki is a project driven by the team's love for innovation through UX design, and is driven by passion, not the desire to generate
+    revenue.
+  </span>
+  <br />
+  <br />
+  <span class="small-subtitle">Hibiki is fully open-source and is licensed under the zlib/libpng license.</span>
+</div>
