89335174e71ab81ac414daf945e396aff91add0f
Espi <espi@riseup.net>
2023-07-22 20:39:35 -0500

    chore: add initial sveltekit structure for web/ (WIP)

---
+++ apps/web/.eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  extends: ["plugin:svelte/recommended", "../../.eslintrc.cjs"],
+  parserOptions: {
+    extraFileExtensions: [".svelte"],
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
+};
