bc1ee39b147ab19b2ffaccaa3fd2d60214fbf46b
Espi <espi@riseup.net>
2023-07-26 02:32:14 -0500

    chore: move bot to package, cleanup bot

---
+++ packages/bot/src/index.ts
@@ -0,0 +1,15 @@
+import { HibikiShardingManager } from "./classes/Sharder.js";
+import { sanitizedEnv } from "$shared/env.js";
+import path from "node:path";
+import url from "node:url";
+
+// Loads i18n
+import "$shared/i18n.js";
+
+// Searches for the root Hibiki file
+const pathDirname = path.dirname(url.fileURLToPath(import.meta.url));
+const HIBIKI_INDEX_FILE = `${pathDirname}/hibiki.${sanitizedEnv.isProduction ? "js" : "ts"}`;
+
+// Creates a new sharding manager
+const manager = new HibikiShardingManager(HIBIKI_INDEX_FILE, sanitizedEnv.TOKEN, "auto");
+manager.spawn();
---
+++ packages/bot/src/utils/constants.ts
@@ -0,0 +1,22 @@
+import { sanitizedEnv } from "$shared/env.js";
+
+// Are we actually in production or not?
+export const IS_PRODUCTION = sanitizedEnv.isProduction;
+
+// A regex for file types that can be modules
+export const moduleFiletypeRegex = /\.(cjs|mjs|js|mts|cts|ts)$/i;
+
+// The version of Hibiki currently running
+export const hibikiVersion = sanitizedEnv.npm_package_version ?? "develop";
+
+// Hex colors used throughout the app
+export enum HibikiColors {
+  // Gold primary
+  GENERAL = 0xff_b0_00,
+
+  // Success pink
+  SUCCESS = 0xdc_26_7f,
+
+  // Orange error
+  ERROR = 0xfe_61_00,
+}
