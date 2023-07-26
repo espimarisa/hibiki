0a35dac4bf34e512487f5eeef852e5678dfd7895
Espi Marisa <hi@espi.me>
2023-07-26 02:32:14 -0500

    chore: move bot to package, cleanup bot

---
+++ packages/bot/src/classes/Client.ts
@@ -0,0 +1,52 @@
+import type { HibikiCommand } from "./Command.js";
+import type { HibikiEvent } from "./Event.js";
+import { loadCommands, loadEvents } from "../utils/loader.js";
+import { sanitizedEnv } from "$shared/env.js";
+import { logger } from "$shared/logger.js";
+import { DatabaseManager } from "$shared/prisma.js";
+import { Client, type ClientOptions } from "discord.js";
+import path from "node:path";
+import url from "node:url";
+import util from "node:util";
+
+// __dirname replacement in ESM
+const pathDirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+// Directories to crawl
+const COMMANDS_DIRECTORY = path.join(pathDirname, "../commands");
+const EVENTS_DIRECTORY = path.join(pathDirname, "../events");
+
+export class HibikiClient extends Client {
+  readonly commands: Map<string, HibikiCommand> = new Map();
+  readonly events: Map<string, HibikiEvent> = new Map();
+
+  readonly db: DatabaseManager = new DatabaseManager();
+
+  constructor(options: ClientOptions) {
+    super(options);
+    // Logs errors
+    this.on("error", (err) => {
+      logger.error(util.inspect(err));
+    });
+  }
+
+  // Starts Hibiki
+  public init() {
+    try {
+      this.login(sanitizedEnv.TOKEN);
+
+      this.once("ready", async () => {
+        // Loads all commands and events
+        await loadCommands(this, COMMANDS_DIRECTORY);
+        await loadEvents(this, EVENTS_DIRECTORY);
+
+        // Logs statistics and other stuff
+        logger.info("Logged in to Discord");
+        logger.info(`${this.commands.size} commands loaded`);
+        logger.info(`${this.events.size} events loaded`);
+      });
+    } catch (error) {
+      logger.error(`An error occured while starting: ${util.inspect(error)}`);
+    }
+  }
+}
