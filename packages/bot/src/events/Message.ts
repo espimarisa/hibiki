2f13c9dfd8023d2c956b953a077246e7d58542bb
Espi Marisa <contact@espi.me>
2023-08-09 23:08:36 -0500

    chore: lint

--- packages/bot/src/events/Message.ts
+++ packages/bot/src/events/Message.ts
@@ -1,10 +1,12 @@
 import { HibikiEvent } from "../classes/Event.js";
-import { Message } from "discord.js";
+import { ChannelType, Message } from "discord.js";
 
 export class HibikiMessageEvent extends HibikiEvent {
   events: HibikiEventListener[] = ["messageCreate"];
 
+  // Not implemented yet
+  // eslint-disable-next-line @typescript-eslint/require-await
   public async run(_event: HibikiEventListener[], msg: Message) {
-    if (!msg.content || msg.author.bot) return;
+    if (!msg.content || msg.author.bot || msg.channel.type !== ChannelType.GuildText) return;
   }
 }
