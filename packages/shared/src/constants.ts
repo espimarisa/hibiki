9896c54ac78f32025025e08f0141bab1e2b05e3a
Espi Marisa <contact@espi.me>
2023-08-18 20:55:35 -0500

    feat(web): add static redirects for donate, invite, support, and github

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -1,3 +1,5 @@
+import { sanitizedEnv } from "./env.js";
+
 // A regex for file types that can be modules
 export const moduleFiletypeRegex = /\.(cjs|mjs|js|mts|cts|ts)$/i;
 
@@ -12,3 +14,17 @@ export enum HibikiColors {
   // Orange error
   ERROR = 0xfe_61_00,
 }
+
+// Support invite
+export const HibikiSupportInvite = "https://discord.gg/VpRscV3mdn";
+
+// Invite permissions
+export const HibikiInvitePermissions = 28_307_378_007_798;
+
+// Invite scope
+export const HibikiInviteScope = ["bot", "applications.commands"];
+
+// Invite URI
+export const HibikiInviteURI =
+  "https://discord.com/oauth2/authorize?&client_id=" +
+  `${sanitizedEnv.BOT_OAUTH_CLIENT_ID}&scope=${encodeURIComponent(HibikiInviteScope.join(" "))}&permissions=${HibikiInvitePermissions}`;
