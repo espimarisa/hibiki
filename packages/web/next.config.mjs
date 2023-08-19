7233a5c49282749224728874173667708dd7896c
Espi Marisa <contact@espi.me>
2023-08-18 20:55:35 -0500

    feat(web): add static redirects for donate, invite, support, and github

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import("next").NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    // Fixes a really annoying Next.JS bug that Vercel doesn't want to fix
+    // https://github.com/vercel/next.js/discussions/32237#discussioncomment-4793595
+    config.resolve.extensionAlias = {
+      ".js": [".ts", ".tsx", ".js"],
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
---
+++ packages/web/src/app/github/page.tsx
@@ -0,0 +1,6 @@
+import { RedirectType } from "next/dist/client/components/redirect.js";
+import { redirect } from "next/navigation.js";
+
+export default function page() {
+  return redirect("https://github.com/espimarisa/hibiki", RedirectType.push);
+}
