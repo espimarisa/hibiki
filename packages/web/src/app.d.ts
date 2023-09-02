fd29c9f4d51fad2a0898add76af4f9491e1ad9db
Espi Marisa <contact@espi.me>
2023-09-02 18:10:20 -0500

    chore: fix svelte linting, lint; add .vscode/settings.json

--- packages/web/src/app.d.ts
+++ packages/web/src/app.d.ts
@@ -36,29 +36,3 @@ declare namespace App {
     session: Session | null;
   }
 }
-
-type SvelteMetadata = {
-  // Page title
-  title?: string;
-
-  // Page description
-  description?: string;
-
-  // Embed image
-  image?: string;
-
-  // Alt text for the image
-  imageAlt?: string;
-
-  // Metadata URL
-  url?: string;
-
-  // The type of content
-  type?: string;
-
-  // A Twitter card URL
-  twitterCard?: string;
-
-  // The color to theme browser UI in
-  themeColor?: string;
-};
