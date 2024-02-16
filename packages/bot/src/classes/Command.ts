5af58572cfb65ae0f553ef8def9e1b969d4c0458
Espi Marisa <contact@espi.me>
2024-02-16 09:13:12 -0600

    feat: i18next, command name and description localization

--- packages/bot/src/classes/Command.ts
+++ packages/bot/src/classes/Command.ts
@@ -2,7 +2,6 @@ import type { HibikiClient } from "$classes/Client.ts";
 import type { APIApplicationCommandOption } from "discord-api-types/v10";
 import { CommandInteraction, ApplicationCommandType } from "discord.js";
 
-// TODO: Expand upon this, enable localisation somehow, etc
 export abstract class HibikiCommand {
   // The type of interaction type. Defaults to CHAT_INPUT.
   interactionType?: ApplicationCommandType = ApplicationCommandType.ChatInput;
@@ -13,24 +12,11 @@ export abstract class HibikiCommand {
   // Whether or not an interaction can only be seen by the runner
   ephemeral = false;
 
-  // A short description of a command
-  abstract description: string;
-
   // Creates a new Hibiki command
   protected constructor(
     protected bot: HibikiClient,
     public name: string,
   ) {}
-
-  // Converts a Hibiki command to Discord API-compatible JSON
-  public toJSON() {
-    return {
-      name: this.name.toLowerCase(),
-      description: this.interactionType === ApplicationCommandType.ChatInput ? this.description : undefined,
-      options: this.options,
-      type: this.interactionType,
-    };
-  }
 
   // Runs a command via the interaction gateway
   public runWithInteraction?(interaction: CommandInteraction, ...args: string[]): Promise<void>;
@@ -41,11 +27,3 @@ export abstract class HibikiCommand {
 
 // A callable type for an abstract Hibiki command
 export type CallableHibikiCommand = new (bot: HibikiClient, name: string) => HibikiCommand;
-
-// Valid Command JSON type
-export interface HibikiCommandJSON {
-  name: string;
-  description?: string;
-  options?: APIApplicationCommandOption[];
-  type?: ApplicationCommandType;
-}
--- packages/bot/src/commands/testing.ts
+++ packages/bot/src/commands/testing.ts
@@ -1,7 +1,7 @@
 import type { ChatInputCommandInteraction } from "discord.js";
 import { HibikiCommand } from "$classes/Command.ts";
 
-export class HibikiTestCommand extends HibikiCommand {
+export class HibikiTestingCommand extends HibikiCommand {
   public async runWithInteraction(interaction: ChatInputCommandInteraction) {
     await interaction.followUp({
       embeds: [
