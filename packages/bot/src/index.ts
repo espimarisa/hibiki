74f6e319174fe6b330d44c77a40d41b2127d35b8
Espi Marisa <contact@espi.me>
2024-02-17 02:28:15 -0600

    chore: clean up loader typings

--- packages/bot/src/index.ts
+++ packages/bot/src/index.ts
@@ -2,9 +2,6 @@ import { HibikiShardingManager } from "$classes/Sharder.ts";
 import env from "$shared/env.ts";
 
 import "$shared/i18n.ts";
-
-// Use .ts in development. .js in production
-// NOTE: In Bun, this doesn't matter, but I'm leaving it as is just in case we decide to compile later
 const HIBIKI_INDEX_FILE = `${import.meta.dir}/hibiki.ts`;
 
 // Creates a new sharding manager
