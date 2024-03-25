c99167eb7a7bbbc71d0a1caaaddd2c5f78e781e4
Espi Marisa <contact@espi.me>
2024-03-25 06:19:38 -0500

    feat: add about command; add format, timeSince, and timestamp utilities

--- packages/bot/src/commands/about.ts
+++ packages/bot/src/commands/about.ts
@@ -1,16 +1,43 @@
-import { type ChatInputCommandInteraction, version } from "discord.js";
+import { type ChatInputCommandInteraction, type EmbedField, version } from "discord.js";
 
 import { HibikiCommand } from "$classes/Command.ts";
 import { fetchTotalCachedGuilds, fetchTotalCachedUsers } from "$classes/Sharder.ts";
 import { HibikiColors } from "$shared/constants.ts";
 import env from "$shared/env.ts";
 import { t } from "$shared/i18n.ts";
+import { getTimeSince } from "$utils/format.ts";
+import { localizeTimeSince } from "$utils/localize.ts";
+
+// Generates a timestamp upon process startup
+const startupTimestamp = new Date();
 
 export class HibikiPingCommand extends HibikiCommand {
   public async runWithInteraction(interaction: ChatInputCommandInteraction, locale: string) {
     // Gets the amount of cached guilds and users
     const totalCachedGuilds = await fetchTotalCachedGuilds(this.bot.shard);
     const totalCachedUsers = await fetchTotalCachedUsers(this.bot.shard);
+    const fields: EmbedField[] = [];
+
+    const uptime = getTimeSince(startupTimestamp, new Date());
+    const localizedUptime = localizeTimeSince(uptime, interaction.locale);
+
+    // Push cached guilds
+    if (totalCachedGuilds) {
+      fields.push({
+        name: t("COMMAND_ABOUT_CACHED_GUILDS", { lng: locale }),
+        value: totalCachedGuilds.toString(),
+        inline: true,
+      });
+    }
+
+    // Push cached users
+    if (totalCachedUsers) {
+      fields.push({
+        name: t("COMMAND_ABOUT_CACHED_USERS", { lng: locale }),
+        value: totalCachedUsers.toString(),
+        inline: true,
+      });
+    }
 
     await interaction.followUp({
       embeds: [
@@ -19,20 +46,10 @@ export class HibikiPingCommand extends HibikiCommand {
           description: t("COMMAND_ABOUT_DETAILS", { username: this.bot.user?.username, lng: locale }),
           color: HibikiColors.GENERAL,
           fields: [
+            ...fields,
             {
-              name: t("COMMAND_ABOUT_CACHED_GUILDS", { lng: locale }),
-              value: totalCachedGuilds?.toString() ?? "idk",
-              inline: true,
-            },
-            {
-              name: t("COMMAND_ABOUT_CACHED_USERS", { lng: locale }),
-              value: totalCachedUsers?.toString() ?? "idk",
-              inline: true,
-            },
-            {
-              // TODO: Localize
               name: t("COMMAND_ABOUT_UPTIME", { lng: locale }),
-              value: `${Math.floor(process.uptime()).toString()} seconds`,
+              value: localizedUptime,
               inline: true,
             },
             {
---
+++ packages/bot/src/utils/localize.ts
@@ -0,0 +1,50 @@
+import type { Duration } from "date-fns";
+
+import { t } from "$shared/i18n.ts";
+
+interface LocalizedDuration {
+  years?: string;
+  months?: string;
+  weeks?: string;
+  days?: string;
+  hours?: string;
+  minutes?: string;
+  seconds?: string;
+}
+
+// Returns a formatted string (ex. 2 months, 1 day, 3 minutes, 60 seconds)
+export function localizeTimeSince(time: Duration, locale: string, disabledItems?: LocalizedDuration) {
+  const formattedDuration: Map<keyof LocalizedDuration, string> = new Map();
+
+  // Number of years
+  if (time.years && !disabledItems?.years) {
+    formattedDuration.set("years", t("YEARS", { count: time.years, lng: locale }));
+  }
+
+  // Number of Months
+  if (time.months && !disabledItems?.months) {
+    formattedDuration.set("months", t("MONTHS", { count: time.months, lng: locale }));
+  }
+
+  // Number of weeks
+  if (time.weeks && !disabledItems?.weeks) {
+    formattedDuration.set("weeks", t("WEEKS", { count: time.weeks, lng: locale }));
+  }
+
+  // Number of days
+  if (time.days && !disabledItems?.days) {
+    formattedDuration.set("days", t("DAYS", { count: time.days, lng: locale }));
+  }
+
+  // Number of minutes
+  if (time.minutes && !disabledItems?.minutes) {
+    formattedDuration.set("minutes", t("MINUTES", { count: time.minutes, lng: locale }));
+  }
+
+  // Number of seconds
+  if (time.seconds && !disabledItems?.seconds) {
+    formattedDuration.set("seconds", t("SECONDS", { count: time.seconds, lng: locale }));
+  }
+
+  return [...formattedDuration.values()].join(", ");
+}
