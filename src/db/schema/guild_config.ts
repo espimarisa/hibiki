262649e8fb0e400073aa17a4a906c6a0182fd01a
Espi Marisa <contact@espi.me>
2024-08-29 04:17:20 -0500

    db: add postgresql + drizzle support

---
+++ src/db/schema/guild_config.ts
@@ -0,0 +1,6 @@
+import { pgTable, text, uuid } from "drizzle-orm/pg-core";
+
+export const guildConfig = pgTable("guild_config", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  guild_id: text("guild_id").notNull(),
+});
