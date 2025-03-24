5168f5ef6dec16526a02801769e74beb7046abd2
Espi Marisa <contact@espi.me>
2025-03-24 14:09:30 -0500

    feat: status cycling support

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,16 +5,21 @@
  */
 
 import { bot } from "$root/bot.ts";
+import { env } from "$utils/env.ts";
+import { cycleStatuses } from "$utils/status.ts";
 
 /**
  * Runs a ready listener when the ready event is fired.
  */
 
-bot.events.ready = ({ user, shardId }) => {
+bot.events.ready = async ({ user, shardId }) => {
 	// Log when all shards are connected
 	if (shardId === bot.gateway.lastShardId) {
-		bot.logger.info(
-			`Connected to Discord as ${user.username}#${user.discriminator}`,
-		);
+		bot.logger.info(`Connected to Discord as ${user.tag}`);
+
+		// Cycle through statuses if any are set
+		if (env.DISCORD_STATUSES && env.DISCORD_STATUSES.split(", ").length > 0) {
+			await cycleStatuses(env.DISCORD_STATUSES.split(", "));
+		}
 	}
 };
---
+++ src/utils/status.ts
@@ -0,0 +1,46 @@
+/**
+ * @file status
+ * @description Utilities for setting and updating the bot's status(es).
+ * @author Espi Marisa <contact@espi.me>
+ */
+
+import { bot } from "$root/bot.ts";
+import { ActivityTypes } from "@discordeno/types";
+let activityState = 0;
+
+/**
+ * Updates the bot's status every 5 minutes.
+ * @param statuses An array of statuses to cycle through.
+ */
+
+export async function cycleStatuses(statuses: string[]) {
+	// Don't update anything if there is no status
+	if (statuses?.length === 0) {
+		return;
+	}
+
+	// Calculates what status to set
+	activityState = (activityState + 1) % statuses.length;
+	const presence = statuses[activityState];
+	if (!presence) {
+		return;
+	}
+
+	// Updates the status
+	await bot.gateway.editBotStatus({
+		status: "online",
+		activities: [
+			{
+				name: presence,
+				state: presence,
+				type: ActivityTypes.Custom,
+			},
+		],
+	});
+
+	// Only run a loop if there is more than 1 status
+	if (statuses.length > 1) {
+		// Cycle through each status every 5 minutes
+		setInterval(cycleStatuses, 30000, statuses);
+	}
+}
